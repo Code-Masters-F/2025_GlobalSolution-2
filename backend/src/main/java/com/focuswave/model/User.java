d5081b75aadec2e68b142a3c5f2ac4aded5af907
Guilherme-Avellar <guilhermeavellar25@gmail.com>
2025-11-21 18:00:06 -0300

    feat: getters e settes, and constructor

--- backend/src/main/java/com/focuswave/model/User.java
+++ backend/src/main/java/com/focuswave/model/User.java
@@ -5,4 +5,43 @@ public class User {
     private String name;
     private String passwordHash;
     private String email;
+
+    public User(int id, String name, String passwordHash, String email) {
+        this.id = id;
+        this.name = name;
+        this.passwordHash = passwordHash;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
